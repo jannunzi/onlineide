59e6ba1f89a0c38845e3e0719cd48882e7366960
jannunzi <jannunzi@gmail.com>
2014-10-06 10:12:39 -0400

    To openshift

--- server.js
+++ server.js
@@ -1,159 +1,81 @@
-#!/bin/env node
-//  OpenShift sample Node application
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+// load webservice and database libraries
 var express = require('express');
-var fs      = require('fs');
+var mongojs = require('mongojs');
 
+// instantiate both libraries and connecto to the cs5610353 database
+var app = express();
+var db = mongojs("cs5610353", ["serviceClients"]);
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+// serve static content (html, css, js) in the public directory
+app.use(express.static(__dirname + '/public'));
 
-    //  Scope.
-    var self = this;
+// configure express to parse JSON in the body of an HTTP request
+app.use(express.bodyParser());
 
+app.get("/env", function (req, res) {
+    res.json(process.env);
+});
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+// map incoming HTTP URL patterns to execute various functions
+// handle HTTP GET request to read all serviceClients from the database
+app.get("/serviceClients", function (req, res) {
+	db.serviceClients.find(function (err, docs) {
+		res.json(docs);
+	});
+});
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// handle HTTP POST request to insert new serviceClients into the database
+app.post("/serviceClients", function (req, res) {
+	// the serviceClient is in the body of the HTTP request
+	var svc = req.body;
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+	// insert new serviceClient object into the database collection serviceClients
+	db.serviceClients.insert(req.body, function (err, doc) {
+		// respond with the new object that has been inserted
+		res.json(doc);
+	});
+});
 
+// handle HTTP GET request for a single serviceClient with :id parameter
+app.get("/serviceClients/:id", function (req, res) {
+	// parse id from the path parameter
+	var id = req.params.id;
+	// select the single document from the database
+	db.serviceClients.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+		// respond with the document retrieved from the database
+		res.json(doc);
+	});
+});
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+// handle HTTP PUT request to update serviceClient instance with :id parameter
+app.put("/serviceClients/:id", function (req, res) {
+	db.serviceClients.findAndModify({
+		// find the object by id
+		query: { _id: mongojs.ObjectId(req.params.id) },
+		// new values are in req.body, update it's name
+		update: { $set: { name: req.body.name } },
+		// single one
+		new: true
+	}, function(err, doc, lastErrorObject) {
+		// respond with the new document
+		res.json(doc);
+	});	
+});
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+// handle HTTP DELETE request to remove a serviceClient with :id parameter
+app.delete("/serviceClients/:id", function (req, res) {
+	// parse id from the path parameter
+	var id = req.params.id;
+	// find the document by id and remove it
+	db.serviceClients.remove({ _id: mongojs.ObjectId(id) },
+		function (err, doc) {
+			// respond with number of documents affected
+			res.json(doc);
+		});
+});
 
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+// listen to port 3000 in localhost
+app.listen(3000);
